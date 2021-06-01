961c9f750498852e6f72915189ebf1f77be77feb
AhmetGENCOGLU <agncglu@gmail.com>
2021-06-01 18:38:49 +0300

    auth

--- helpers/errorHandler.js
+++ helpers/errorHandler.js
@@ -1,6 +1,6 @@
 function errorHandler(err, req, res, next) {
     if(err.name === "UnauthorizedError" ){
-        return res.status(401).json({message:"The user isnt authorized..."})
+        return res.status(401).json({message:"The user isnt authorized."})
     }
     if(err.name === "ValidationError" ){
         return res.status(401).json({message:err})
